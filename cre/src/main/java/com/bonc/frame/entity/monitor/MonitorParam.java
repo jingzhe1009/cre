94ba2bb746bfdf04b30044a863cd0f1a34b93402
ljz <253753997@163.com>
2022-04-24 15:35:19 +0800

    lujingzhe 监控

---
+++ cre/src/main/java/com/bonc/frame/entity/monitor/MonitorParam.java
@@ -0,0 +1,68 @@
+package com.bonc.frame.entity.monitor;
+
+
+public class MonitorParam {
+	
+	//产品id
+	private String productId;
+	
+	//渠道id
+	private String channelId;
+	
+	//模型id
+	private String modelId;
+	
+	//调用方式
+	private String type;
+	
+	//年1月2日3
+	private String cycleId;
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getChannelId() {
+		return channelId;
+	}
+
+	public void setChannelId(String channelId) {
+		this.channelId = channelId;
+	}
+
+	public String getModelId() {
+		return modelId;
+	}
+
+	public void setModelId(String modelId) {
+		this.modelId = modelId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getCycleId() {
+		return cycleId;
+	}
+
+	public void setCycleId(String cycleId) {
+		this.cycleId = cycleId;
+	}
+
+	
+	
+	
+	
+	
+	
+
+}
